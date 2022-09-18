58068020784c91d6c29b766f3d7be7458cb2f880
Cramble <69367193+Cramblioni@users.noreply.github.com>
2022-09-19 00:45:36 +0100

    attempt to fix build errors

--- playground.nim
+++ playground.nim
@@ -46,7 +46,7 @@ settings:
 
 routes:
   post "/api/execute":
-    headers = newStringTable(modeCaseSensitive)
+    var headers = newStringTable(modeCaseSensitive)
     headers["Content-Type"] = "application/json"
     resp(execute(request.body))
 
